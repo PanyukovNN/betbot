6851f8666cd2a92b77ec14e356aa78299b63cd66
Nickolay <zylex77@gmail.com>
2019-11-21 18:46:07 +0300

    fix StatisticsAnalyser

--- src/main/java/com/zylex/betbot/StatisticsApplication.java
+++ src/main/java/com/zylex/betbot/StatisticsApplication.java
@@ -1,7 +1,6 @@
 package com.zylex.betbot;
 
 import com.zylex.betbot.controller.Repository;
-import com.zylex.betbot.service.Day;
 import com.zylex.betbot.service.bet.rule.RuleNumber;
 import com.zylex.betbot.service.statistics.ResultScanner;
 import com.zylex.betbot.service.statistics.StatisticsAnalyser;
@@ -11,16 +10,17 @@ import java.time.LocalDate;
 public class StatisticsApplication {
 
     public static void main(String[] args) {
+        RuleNumber ruleNumber = RuleNumber.RULE_ONE;
+
         LocalDate startDate = LocalDate.of(2019, 11, 11);
         LocalDate endDate = LocalDate.now().minusDays(1);
 
-//        new StatisticsAnalyser(
-//            new ResultScanner(
-//                new Repository(
-//                    Day.TODAY,
-//                    RuleNumber.RULE_ONE
-//                )
-//            )
-//        ).analyse(startDate, endDate);
+        new StatisticsAnalyser(
+            new ResultScanner(
+                new Repository(
+                    ruleNumber
+                )
+            )
+        ).analyse(startDate, endDate);
     }
 }
