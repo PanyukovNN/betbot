269641ab1ea40b2637bf4545b0693dafcb93162b
Nickolay <zylex77@gmail.com>
2020-01-02 07:32:34 +0300

    improve logger

--- src/main/java/com/zylex/betbot/controller/logger/ConsoleLogger.java
+++ src/main/java/com/zylex/betbot/controller/logger/ConsoleLogger.java
@@ -21,7 +21,7 @@ public abstract class ConsoleLogger {
     static {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm a");
         LocalDateTime startDateTime = LocalDateTime.now();
-        logOutput = "\n\n" + StringUtils.repeat("-", 100) + "\n"
+        logOutput = "\n" + StringUtils.repeat("*", 50) + "\n"
                 + String.format("Bot started at: %s", startDateTime.format(formatter))
                 + "\n" + StringUtils.repeat("-", 50) + "\n";
     }
@@ -44,9 +44,9 @@ public abstract class ConsoleLogger {
 
     private synchronized static void addToLog(String line) {
         if (line.contains("\b")) {
-            int a = StringUtils.countMatches(line, "\b");
-            int b = logOutput.lastIndexOf("\n");
-            logOutput = logOutput.substring(0, Math.max(logOutput.length() - a, b + 1))
+            int backspaces = StringUtils.countMatches(line, "\b");
+            int lastNewLineIndex = logOutput.lastIndexOf("\n");
+            logOutput = logOutput.substring(0, Math.max(logOutput.length() - lastNewLineIndex, backspaces + 1))
                     + line.replace("\b", "");
         } else {
             logOutput += line;
@@ -61,6 +61,10 @@ public abstract class ConsoleLogger {
         logOutput += "\n" + message;
     }
 
+    public static void endMessage() {
+        writeInLine(String.format("\nBot work completed in %s", computeTime(programStartTime.get())));
+    }
+
     void writeLineSeparator() {
         String line = "\n" + StringUtils.repeat("-", 50);
         writeInLine(line);
@@ -71,7 +75,7 @@ public abstract class ConsoleLogger {
         addToLog(line);
     }
 
-    synchronized void writeInLine(String line) {
+    static synchronized void writeInLine(String line) {
         System.out.print(line);
         addToLog(line);
     }
@@ -85,7 +89,7 @@ public abstract class ConsoleLogger {
         }
     }
 
-    String computeTime(long startTime) {
+    static String computeTime(long startTime) {
         long millis = System.currentTimeMillis() - startTime;
         String time = String.format("%02d min. %02d sec.",
                 TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1),
