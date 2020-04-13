be462e55c10e38b94ffe357debce6a4512c71995
Nickolay <zylex77@gmail.com>
2020-04-13 13:00:18 +0300

    for Heroku_2

--- src/main/java/com/zylex/betbot/controller/logger/StatisticsConsoleLogger.java
+++ src/main/java/com/zylex/betbot/controller/logger/StatisticsConsoleLogger.java
@@ -1,49 +1,45 @@
 package com.zylex.betbot.controller.logger;
 
-import com.zylex.betbot.model.Rule;
-
-import java.time.LocalDate;
-
 /**
  * Log StatisticsAnalyser.
  */
 public class StatisticsConsoleLogger extends ConsoleLogger{
 
-    /**
-     * Log start message.
-     * @param startDate - start date.
-     * @param endDate - end date.
-     */
-    public synchronized void startLogMessage(LocalDate startDate, LocalDate endDate) {
-        writeInLine(String.format("\nAnalyse statistics for period from %s to %s", startDate, endDate));
-        writeLineSeparator();
-    }
-
-    /**
-     * Log formatted statistics.
-     * @param rule - specified rule.
-     * @param gamesNumber1 - number of total games.
-     * @param firstWins1 - number of total first win games.
-     * @param ties1 - number of total tie games.
-     * @param secondWins1 - number of total second win games.
-     * @param noResults1 - number of total no result games.
-     * @param gamesNumber2 - number of games for leagues from file.
-     * @param firstWins2 - number of first win games for leagues from file.
-     * @param ties2 - number of total tie games for leagues from file.
-     * @param secondWins2 - number of total second win games for leagues from file.
-     * @param noResults2 - number of total no result games for leagues from file.
-     */
-    public void logStatistics(Rule rule,
-                              int gamesNumber1, int firstWins1, int ties1, int secondWins1, int noResults1,
-                              int gamesNumber2, int firstWins2, int ties2, int secondWins2, int noResults2) {
-        writeInLine("\nGames for " + rule.getName() + ":");
-        writeInLine(String.format("\n%10s%4d | %-4d", "Total - ", gamesNumber1, gamesNumber2));
-        writeInLine(String.format("\n%10s%4d | %-4d", "1W - ", firstWins1, firstWins2));
-        writeInLine(String.format("\n%10s%4d | %-4d", " X - ", ties1, ties2));
-        writeInLine(String.format("\n%10s%4d | %-4d", "W2 - ", secondWins1, secondWins2));
-        if (noResults1 > 0 || noResults2 > 0) {
-            writeInLine(String.format("\n%10s%4d | %-4d", "N/R - ", noResults1, noResults2));
-        }
-        writeLineSeparator();
-    }
+//    /**
+//     * Log start message.
+//     * @param startDate - start date.
+//     * @param endDate - end date.
+//     */
+//    public synchronized void startLogMessage(LocalDate startDate, LocalDate endDate) {
+//        writeInLine(String.format("\nAnalyse statistics for period from %s to %s", startDate, endDate));
+//        writeLineSeparator();
+//    }
+//
+//    /**
+//     * Log formatted statistics.
+//     * @param rule - specified rule.
+//     * @param gamesNumber1 - number of total games.
+//     * @param firstWins1 - number of total first win games.
+//     * @param ties1 - number of total tie games.
+//     * @param secondWins1 - number of total second win games.
+//     * @param noResults1 - number of total no result games.
+//     * @param gamesNumber2 - number of games for leagues from file.
+//     * @param firstWins2 - number of first win games for leagues from file.
+//     * @param ties2 - number of total tie games for leagues from file.
+//     * @param secondWins2 - number of total second win games for leagues from file.
+//     * @param noResults2 - number of total no result games for leagues from file.
+//     */
+//    public void logStatistics(Rule rule,
+//                              int gamesNumber1, int firstWins1, int ties1, int secondWins1, int noResults1,
+//                              int gamesNumber2, int firstWins2, int ties2, int secondWins2, int noResults2) {
+//        writeInLine("\nGames for " + rule.getName() + ":");
+//        writeInLine(String.format("\n%10s%4d | %-4d", "Total - ", gamesNumber1, gamesNumber2));
+//        writeInLine(String.format("\n%10s%4d | %-4d", "1W - ", firstWins1, firstWins2));
+//        writeInLine(String.format("\n%10s%4d | %-4d", " X - ", ties1, ties2));
+//        writeInLine(String.format("\n%10s%4d | %-4d", "W2 - ", secondWins1, secondWins2));
+//        if (noResults1 > 0 || noResults2 > 0) {
+//            writeInLine(String.format("\n%10s%4d | %-4d", "N/R - ", noResults1, noResults2));
+//        }
+//        writeLineSeparator();
+//    }
 }
